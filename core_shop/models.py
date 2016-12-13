fe01e37221c699a9c64f5a56d0526b9ee31fb71a
Margarita <margo.himera@yandex.ru>
2016-12-13 23:28:53 +0300

    Models was created

--- core_shop/models.py
+++ core_shop/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True, help_text='Unique value for product page URL, '
+                                                                  'created from name.')
+
+    class Meta:
+        db_table = 'categories'
+        ordering = ['name']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __unicode__(self):
+        return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('catalog_category', (), { 'category_slug': self.slug })
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', verbose_name="Категория")
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    slug = models.SlugField(max_length=255, unique=True, help_text='Unique value for product page URL, '
+                                                                   'created from name.')
+    description = models.TextField(blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    count = models.PositiveIntegerField(verbose_name="Количество на складе")
+    available = models.BooleanField(default=True, verbose_name="Доступен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        ordering = ['name']
+        index_together = [['id', 'slug']]
+
+    def __unicode__(self):
+        return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('catalog_product', (), { 'product_slug': self.slug })
