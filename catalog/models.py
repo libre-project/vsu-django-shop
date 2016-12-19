6652d0a81f014cfdd4cbc7d6659fee0acd8e78a8
citizensix <eugene0tall@gmail.com>
2016-12-19 23:49:31 +0300

    Fix for

--- catalog/models.py
+++ catalog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 #Модель категории
-class category(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=30)
     slug = models.SlugField(max_length = 50, unique = True, help_text='Уникальное значение для URL продукта')
     description = models.TextField(max_length = 2000)
     is_active = models.BooleanField(default = True)
-    meta_keywords = models.CharField("Meta Keywords", max_length = 255. help_text = 'Comma-delimited set of SEO keywords for meta tag')
+    meta_keywords = models.CharField("Meta Keywords", max_length = 255, help_text = 'Comma-delimited set of SEO keywords for meta tag')
     meta_description = models.CharField("Meta Description", max_length = 255, help_text = 'Content for description meta tag')
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
--- darknet_shop/settings.py
+++ darknet_shop/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core_shop',
-    'catalog'
+    'catalog',
     'cart',
 ]
 
